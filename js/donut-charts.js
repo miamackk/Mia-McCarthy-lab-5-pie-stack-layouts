7b57447dbf86fbba4ff2768da9b5c27ccd8a6068
miamccarthy <miamcc531@gmail.com>
2026-02-17 11:44:10 -0500

    check 0-3 done

--- js/donut-charts.js
+++ js/donut-charts.js
@@ -1,5 +1,82 @@
 const drawDonutCharts = (data) => {
-  // Generate the donut charts here
 
-  
+  const svg = d3.select("#donut")
+    .append("svg")
+      .attr("viewBox", `0 0 ${width} ${height}`);
+
+  const years = [1975, 1995, 2013];
+  const formats = data.columns.filter(format =>
+    format !== "year");
+
+  years.forEach(year => {
+    const donutContainer = svg
+      .append("g")
+        .attr("transform", `translate(${xScale(year)+margin.left}, ${innerHeight/2})`)  
+
+    const yearData = data.find(d => d.year === year);
+
+    const formattedData = []
+
+    formats.forEach(format => {
+      formattedData.push({ 
+        format: format,
+        sales: yearData[format]
+      });
+    });
+
+  const pieGenerator = d3.pie()
+    .value(d => d.sales);
+  const annotatedData = pieGenerator(formattedData)
+
+  const arcGenerator = d3.arc()
+    .startAngle(d => d.startAngle)
+    .endAngle(d => d.endAngle)
+    .innerRadius(60)
+    .outerRadius(100)
+    .padAngle(.02)
+    .cornerRadius(3);
+
+  //append svg group for each year
+  const arcs = donutContainer
+    .selectAll(`.arc-${year}`)
+    .data(annotatedData)
+    .join("g")
+      .attr("class", `arc-${year}`)
+
+      //append path element in each group, draw arcs, set cfill with color scale
+  arcs
+    .append("path")
+      .attr("d", arcGenerator)
+      .attr("fill", d => colorScale(d.data.format))
+
+  arcs
+    .append("text") //append text within each group
+      .text(d => {
+        d["percentage"] = (d.endAngle - d.startAngle) / (2 * Math.PI); //calculate percantage of each arc. store in bound data d["percentage"]
+        return d3.format(".0%")(d.percentage);
+      })
+      .attr("x", d => { //get pos of centroid of each arc, store in d["centroid"], use to set x and y of labels
+        d["centroid"] = arcGenerator
+          .startAngle(d.startAngle)
+          .endAngle(d.endAngle)
+          .centroid();
+        return d.centroid[0]
+      })
+      .attr("y", d => d.centroid[1])
+      .attr("text-anchor", "middle")
+      .attr("dominant-baseline", "middle")
+      .attr("fill", "#f6fafc")
+      .attr("fill-opacity", d => d.percentage < 0.05 ? 0 : 1) // hide labels for segments that are less than 5%
+      .style("font-size", "16px")
+      .style("font-weight", 500)
+
+  donutContainer
+      .append("text")
+        .text(year)
+        .attr("text-anchor", "middle")
+        .attr("dominant-baseline", "middle")
+        .style("font-size", "24px")
+        .style("font-weight", 500);
+
+ });
 };
--- js/stacked-bars.js
+++ js/stacked-bars.js
@@ -1,6 +1,5 @@
 const drawStackedBars = (data) => {
   // Generate the stacked bar chart here
-  
 
   /*******************************/
   /*    Append the containers    */
@@ -12,6 +11,54 @@ const drawStackedBars = (data) => {
   const innerChart = svg
     .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`);
+  //initialize stack layout
 
-  
+  const stackGenerator = d3.stack()
+    .keys(formatsInfo.map(f => f.id))
+    .order(d3.stackOrderDescending)
+    .offset(d3.stackOffsetExpand)
+
+  const annotatedData = stackGenerator(data);
+  console.log("annotatedData bars", annotatedData)
+
+  const minLowerBoundaries = [];
+  const maxUpperBoundaries = [];
+
+  annotatedData.forEach(series => {
+    minLowerBoundaries.push(d3.min(series, d => d[0]));
+    maxUpperBoundaries.push(d3.max(series, d => d[1]));
+  })
+  const minDomain = d3.min(minLowerBoundaries);
+  const maxDomain = d3.max(maxUpperBoundaries);
+
+  const yScale = d3.scaleLinear()
+    .domain([minDomain, maxDomain])
+    .range([innerHeight, 0]);
+
+  annotatedData.forEach(series => {
+    innerChart
+      .selectAll(`.bar-${series.key}`)
+      .data(series)
+      .join("rect")
+        .attr("class", d => `bar-${series.key}`)
+        .attr("x", d => xScale(d.data.year))
+        .attr("y", d => yScale(d[1]))
+        .attr("width", xScale.bandwidth())
+        .attr("height", d => yScale(d[0]) - yScale(d[1]))
+        .attr("fill", colorScale(series.key))
+  })
+
+  const bottomAxis = d3.axisBottom(xScale)
+    .tickValues(d3.range(1975, 2020, 5))
+    .tickSizeOuter(0);
+
+  innerChart
+    .append("g")
+      .attr("transform", `translate(0, ${innerHeight})`)
+      .call(bottomAxis);
+
+  const leftAxis = d3.axisLeft(yScale);
+  innerChart
+    .append("g")
+    .call(leftAxis)
 };
